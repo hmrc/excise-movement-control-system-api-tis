c348a1b2be947d57a7f221012321247da057269d
Mike Bostock <17684266+mpbostock@users.noreply.github.com>
2023-10-25 17:28:40 +0100

    set bootstrap test to test scope

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -5,13 +5,13 @@ object AppDependencies {
 
   val bootStrapPlayVersion = "7.19.0"
 
-  val compile = Seq(
+  val compile: Seq[ModuleID] = Seq(
     ws,
     "uk.gov.hmrc" %% "bootstrap-frontend-play-28" % bootStrapPlayVersion)
 
-  val test = Seq(
-    "uk.gov.hmrc"           %% "bootstrap-test-play-28"     % bootStrapPlayVersion,
-    "org.scalatest"         %% "scalatest" % "3.2.11"       % "test",
-    "com.vladsch.flexmark"  % "flexmark-all" % "0.62.2"     % "test"
+  val test: Seq[ModuleID] = Seq(
+    "uk.gov.hmrc"           %% "bootstrap-test-play-28"     % bootStrapPlayVersion % "test",
+    "org.scalatest"         %% "scalatest"                  % "3.2.11"             % "test",
+    "com.vladsch.flexmark"  % "flexmark-all"                % "0.62.2"             % "test"
   )
 }
